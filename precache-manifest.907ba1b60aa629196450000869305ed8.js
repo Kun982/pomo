20caeb537649307b9622aa8acd46be2362df3f39
Kun982 <kunprivacy@gmail.com>
2021-11-27 17:02:09 +0800

    deploy

---
+++ precache-manifest.907ba1b60aa629196450000869305ed8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013192570f2f9aeba05029b861216a77",
+    "url": "/pomo/alarms/alarm1.mp3"
+  },
+  {
+    "revision": "bfbadf4bb977881ce1eb25f2e492d635",
+    "url": "/pomo/alarms/alarm2.mp3"
+  },
+  {
+    "revision": "46c744f5e53a2259c87f4b029cb43e03",
+    "url": "/pomo/alarms/ya.mp3"
+  },
+  {
+    "revision": "0206abd70e58e6ba6131",
+    "url": "/pomo/css/app.24bef7e0.css"
+  },
+  {
+    "revision": "5bb337c384f50f60c481",
+    "url": "/pomo/css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/pomo/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/pomo/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "cfcc18f8e4b836a6adb7d074e04af23d",
+    "url": "/pomo/img/round-library_music-24px.cfcc18f8.svg"
+  },
+  {
+    "revision": "0163f22949e8bf0bd060802791507449",
+    "url": "/pomo/img/round-list-24px.0163f229.svg"
+  },
+  {
+    "revision": "cb37ebfe74f151b9f73fcd136261ec0f",
+    "url": "/pomo/img/round-schedule-24px.cb37ebfe.svg"
+  },
+  {
+    "revision": "5df50c19a8a9ed1fb10f22cfd7a661fc",
+    "url": "/pomo/img/台北世貿大樓.5df50c19.jpg"
+  },
+  {
+    "revision": "306dcf93c74324aee7914e9513c8b3d9",
+    "url": "/pomo/index.html"
+  },
+  {
+    "revision": "d1267a2ab454cfe8baab",
+    "url": "/pomo/js/about.1ce6e261.js"
+  },
+  {
+    "revision": "0206abd70e58e6ba6131",
+    "url": "/pomo/js/app.4e85513a.js"
+  },
+  {
+    "revision": "5bb337c384f50f60c481",
+    "url": "/pomo/js/chunk-vendors.c13db4d9.js"
+  },
+  {
+    "revision": "aaa364ad973c5ae808b74c83e18894f4",
+    "url": "/pomo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pomo/robots.txt"
+  },
+  {
+    "revision": "cfcc18f8e4b836a6adb7d074e04af23d",
+    "url": "/pomo/usething/icon/round-library_music-24px.svg"
+  },
+  {
+    "revision": "0163f22949e8bf0bd060802791507449",
+    "url": "/pomo/usething/icon/round-list-24px.svg"
+  },
+  {
+    "revision": "cb37ebfe74f151b9f73fcd136261ec0f",
+    "url": "/pomo/usething/icon/round-schedule-24px.svg"
+  }
+]);
